df51fe8b109fa5450e9020602c7b5f0c84e3e60a
tony un <tony un>
2016-06-23 15:48:10 -0400

    First commit for hangman

---
+++ hangman.rb
@@ -0,0 +1,7 @@
+#start
+word = thirsty  # ~> NameError: undefined local variable or method `thirsty' for main:Object
+
+# ~> NameError
+# ~> undefined local variable or method `thirsty' for main:Object
+# ~>
+# ~> /Users/tonyun/Desktop/class/hangman/hangman.rb:2:in `<main>'
