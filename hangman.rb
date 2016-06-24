19817b3dee36484b365fd09b4d6848832ff08210
tony un <tony un>
2016-06-24 15:11:02 -0400

    Baby steps

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,21 @@
-#start
-word = thirsty  # ~> NameError: undefined local variable or method `thirsty' for main:Object
+#start # ~> NameError: undefined local variable or method `thirsty' for main:Object
+word = "Putin"
+guesses = [10]    # ~> NameError: undefined local variable or method `game' for main:Object
+rand_list = [organic, genetically, synthetic]
+puts "Let's play hangman"
+puts "Guess"
+guess = gets.chomp
+
+def initialize (word, guesses)
+    @word = word
+    @guesses = guesses
+end
+
+
+
+
 
 # ~> NameError
-# ~> undefined local variable or method `thirsty' for main:Object
+# ~> undefined local variable or method `game' for main:Object
 # ~>
 # ~> /Users/tonyun/Desktop/class/hangman/hangman.rb:2:in `<main>'
